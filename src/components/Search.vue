3f268660433168347fe77a65e8696977e274ff09
Glenn Martin <glenn.e.martin@gmail.com>
2018-06-10 02:14:12 -0400

    Templating finally doable

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -5,12 +5,18 @@
     <div class="row">row 3</div>
     <form>
       <div class="row">
-        <input type="text" >
+        <input type="text" v-model="term" placeholder="search term">
       </div>
+      <div class="row">
+        <input type="text" v-model="zip" placeholder="zipcode">
+      </div>      
       <div class="row">
         <button type='button' class='bt btn-primary'  @mouseup="searchYelp" >Search</button>
       </div>
     </form>
+    <ul class="list-group">
+      <li v-for="business in searchResults" class="list-group-item">{{ business.alias }}</li>      
+    </ul>
   </div>
 </template>
 
@@ -25,14 +31,19 @@ export default {
       term: '',
       zip: '',
       radius: '',
-      limit:10
+      limit:10,
+      searchResults:[]
     }
   }, methods: {
         searchYelp() {
-          axios.get("http://whereforlunch.herokuapp.com/api/search?zip=07927&term=food&limit=3")
+          //"http://whereforlunch.herokuapp.com/api/search?zip=07927&term=food&limit=3"
+
+          axios.get("http://whereforlunch.herokuapp.com/api/search?zip=" + this.zip + "&term=" + this.term + "&limit=3")
               .then((response)  =>  {
-                console.log(response.data.value);
+                console.log(response.data);
                 //this.jokes = response.data.value;
+                this.searchResults=response.data.businesses
+                //alert('got here');
               }, (error)  =>  {
                 console.log(error);
               })          
